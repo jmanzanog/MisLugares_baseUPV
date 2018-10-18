62d48cfcadaacaaf36b958d683aafedb05e73e1e
jmanzano <j.manzano.g@hotmail.com>
2018-10-17 22:54:24 -0500

    foto usuario

--- app/src/main/java/com/example/mislugares/UsuarioFragment.java
+++ app/src/main/java/com/example/mislugares/UsuarioFragment.java
@@ -2,14 +2,21 @@ package com.example.mislugares;
 
 import android.app.Fragment;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.util.LruCache;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.NetworkImageView;
+import com.android.volley.toolbox.Volley;
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -50,6 +57,21 @@ public class UsuarioFragment extends Fragment {
             }
         });
 
+        // Inicialización Volley (Hacer solo una vez en Singleton o Applicaction)
+        RequestQueue colaPeticiones = Volley.newRequestQueue(getActivity() .getApplicationContext());
+        ImageLoader lectorImagenes = new ImageLoader(colaPeticiones, new ImageLoader.ImageCache() {
+            private final LruCache<String, Bitmap> cache = new LruCache<String, Bitmap>(10);
+            public void putBitmap(String url, Bitmap bitmap) {
+                cache.put(url, bitmap); }
+                public Bitmap getBitmap(String url) {
+                return cache.get(url);
+            }
+        });
+        Uri urlImagen = usuario.getPhotoUrl();
+        if (urlImagen != null) {
+            NetworkImageView fotoUsuario = (NetworkImageView) vista.findViewById(R.id.imagen);
+            fotoUsuario.setImageUrl(urlImagen.toString(), lectorImagenes);
+        }
     return vista;
     }
 }
