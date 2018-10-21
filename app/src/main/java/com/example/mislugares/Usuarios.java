6892f5472a473e1ec1fa5528c994d4cedc9bf430
jmanzano <j.manzano.g@hotmail.com>
2018-10-21 11:30:10 -0500

    Guardar usuario en Firestore

--- app/src/main/java/com/example/mislugares/Usuarios.java
+++ app/src/main/java/com/example/mislugares/Usuarios.java
@@ -2,11 +2,12 @@ package com.example.mislugares;
 
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class Usuarios {
     static void guardarUsuario(final FirebaseUser user) {
         Usuario usuario = new Usuario(user.getDisplayName(), user.getEmail());
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        database.getReference("usuarios/" + user.getUid()).setValue(usuario);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("usuarios").document(user.getUid()).set(usuario);
     }
 }
